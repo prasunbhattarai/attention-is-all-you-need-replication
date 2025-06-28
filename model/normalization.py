10bbfeba31482692dbde8987a2acd91037c4a53d
Prasun Bhattarai <prasunbhattarai2003@gmail.com>
2025-06-28 18:51:54 +0545

    Normalization Layer of the Model

---
+++ model/normalization.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import math
+
+class LayerNormalization(nn.Module):
+    def __init__(self, d_model, e = 1e-6 ):
+        super().__init__()
+        self.d_model = d_model
+        self.e = e
+        self.alpha = nn.Parameter(torch.ones(d_model))
+        self.bias = nn.Parameter(torch.zeros(d_model))
+
+    def forward(self,x):
+        mean = x.mean(dim = -1, keepdim = True)
+        var = x.var(dim = -1, keepdim = True)
+        norm_x = (x - mean)/ torch.sqrt(var + self.e)
+        return self.alpha * norm_x + self.bias
