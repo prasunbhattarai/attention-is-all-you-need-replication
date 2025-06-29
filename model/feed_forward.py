62d212d53330aface31df6ddc9b5dfef9557c199
Prasun Bhattarai <prasunbhattarai2003@gmail.com>
2025-06-29 15:02:45 +0545

    Positional Feed Forward Layer of the model

--- model/feed_forward.py
+++ model/feed_forward.py
@@ -4,7 +4,7 @@ import math
 
 
 class PositionalFeedForward(nn.Module):
-    def __init__(self, d_model, dff, dropout = 0.1):
+    def __init__(self, d_model, dff, dropout):
         super().__init__()
         self.linear1 = nn.Linear(d_model, dff)
         self.dropout = nn.Dropout(dropout)
