d1438cdee81352416f6b6844e31a2334e5c748a1
Prasun Bhattarai <prasunbhattarai2003@gmail.com>
2025-07-06 14:06:34 +0545

    Masks

--- model/masks.py
+++ model/masks.py
@@ -4,3 +4,10 @@ import math
 
 def src_mask(src, pad_token_id = 0):
     return (src != pad_token_id).unsqueeze(1).unsqueeze(2)
+
+def tgt_mask(tgt, pad_token_id = 0):
+    B, T = tgt.shape
+    mask = (tgt != pad_token_id).unsqueeze(1).unsqueeze(2)
+    future_mask = torch.tril(torch.ones(T, T)).bool()
+    future_mask = future_mask.unsqueeze(1).unsqueeze(2)
+    return mask & future_mask
